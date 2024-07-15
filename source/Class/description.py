181a76f45f87f7b8f3374d39b016c7d6a72cf296
Christian2147 <119087529+Christian2147@users.noreply.github.com>
2024-07-14 21:14:46 -0700

    documented description class

--- source/Class/description.py
+++ source/Class/description.py
@@ -13,28 +13,98 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+"""
+    File: description.py
+    Author: Christian Marinkovich
+    Date: 2024-07-14
+    Description:
+    This file contains the class for the description object, which is used to store all of the side panel
+        descriptions for all of the items and upgrades in the shop.
+    These variables are used to create turtle text on the screen.
+"""
+
 
 class Description:
+    """
+        Represents the description object for storing the side panel descriptions.
+
+        Attributes:
+            align (string): The vertical alignment of the text (left, center, right)
+            text (string): The text itself as a string
+            size (int): The font size
+            font (string): The font type (Courier always)
+    """
+
     def __init__(self, align, text, size, font):
+        """
+            Creates a description object
+
+            :param align: The vertical alignment of the text
+            :type align: string
+
+            :param text: The description text
+            :type text: string
+
+            :param size: The font size
+            :type size: int
+
+            :param font: The font type
+            :type font: string
+        """
+
         self.align = align
         self.text = text
         self.size = size
         self.font = font
 
     def __del__(self):
+        """
+            Cleans up the variables from memory once the program has terminated
+
+            :return: None
+        """
+
         del self.align
         del self.text
         del self.size
         del self.font
 
     def get_align(self):
+        """
+            Returns the vertical alignment of the text
+
+            :return: align: The vertical alignment of the text
+            :type: string
+        """
+
         return self.align
 
     def get_text(self):
+        """
+            Returns the description text
+
+            :return: text: The description text
+            :type: string
+        """
+
         return self.text
 
     def get_size(self):
+        """
+            Returns the font size of the description text
+
+            :return: size: The font size
+            :type: int
+        """
+
         return self.size
 
     def get_font(self):
+        """
+            Returns the font type of the description text
+
+            :return: font: The font type
+            :type: string
+        """
+
         return self.font
