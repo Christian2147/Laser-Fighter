c1c778d3eeec70be4fd57347b6cc6be079d5be2d
Christian2147 <119087529+Christian2147@users.noreply.github.com>
2024-08-01 17:34:04 -0700

    Added documentation to laser files

--- source/components/player/HumanLaser.py
+++ source/components/player/HumanLaser.py
@@ -13,12 +13,48 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+"""
+    File: HumanLaser.py
+    Author: Christian Marinkovich
+    Date: 2024-08-01
+    Description:
+    This file contains the logic for the human player's laser sprite in Alien Mode.
+    For some guns, multiple laser sprites are needed.
+"""
+
 import turtle
 from setup.ModeSetupMaster import alien_mode_setup
 
 
 class HumanLaser:
+    """
+        Represents the player's laser in Alien Mode.
+
+        Attributes:
+            _laser (turtle.Turtle()): The player laser sprite
+            laser_update (int): Determines the lasers current pierce value
+
+            scale_factor_x: The scale factor for the x-axis used in fullscreen mode
+            scale_factor_y: The scale factor for the y-axis used in fullscreen mode
+    """
+
     def __init__(self, x, y, scale_factor_x, scale_factor_y):
+        """
+            Creates a Human Laser sprite on the screen.
+
+            :param x: The x-coordinate of the laser
+            :type x: float
+
+            :param y: The y-coordinate of the laser
+            :type y: float
+
+            :param scale_factor_x: The scale factor for the x-axis used in fullscreen mode
+            :type scale_factor_x: float
+
+            :param scale_factor_y: The scale factor for the y-axis used in fullscreen mode
+            :type scale_factor_y: float
+        """
+
         self._laser = turtle.Turtle()
         self.laser.shape(alien_mode_setup.laser_right_texture)
         self.laser.shapesize(0.33 * scale_factor_y, 2 * scale_factor_x)
@@ -45,6 +81,18 @@ class HumanLaser:
         del self.scale_factor_y
 
     def reinstate(self, x, y):
+        """
+            Reuses the existing sprite to fire and spawn the players laser on the screen.
+
+            :param x: The x-coordinate of the laser
+            :type x: float
+
+            :param y: The y-coordinate of the laser
+            :type y: float
+
+            :return: None
+        """
+
         self.laser_update = 20
         self.laser.shape(alien_mode_setup.laser_right_texture)
         self.laser.goto(x, y)
@@ -52,11 +100,25 @@ class HumanLaser:
 
     @property
     def laser(self):
+        """Human Laser Getter"""
         return self._laser
 
     def get_laser_update(self):
+        """
+            Returns the pierce variable for the given laser sprite.
+
+            :return: laser_update: The pierce variable
+            :type: int
+        """
+
         return self.laser_update
 
     def remove(self):
+        """
+            Removes the human laser form the screen.
+
+            :return: None
+        """
+
         self.laser.hideturtle()
         self.laser_update = 0
--- source/components/player/MachinePlayerLaser.py
+++ source/components/player/MachinePlayerLaser.py
@@ -13,12 +13,38 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+"""
+    File: MachinePlayerLaser.py
+    Author: Christian Marinkovich
+    Date: 2024-08-01
+    Description:
+    This file contains the logic for the machine player's laser sprite in Machine Mode.
+    For some machine players, multiple laser sprites are needed.
+"""
+
 import turtle
 from setup.ModeSetupMaster import machine_mode_setup
 
 
 class MachineLaser:
+    """
+        Represents the players laser in Machine Mode.
+
+        Attributes:
+            _laser (turtle.Turtle()): The machine players laser sprite
+    """
+
     def __init__(self, x, y):
+        """
+            Creates a player object.
+
+            :param x: The x-coordinate of the laser
+            :type x: float
+
+            :param y: The y-coordinate of the laser
+            :type y: float
+        """
+
         self._laser = turtle.Turtle()
         self._laser.shape(machine_mode_setup.laser_texture)
         # Ensure that the turtle does not draw lines on the screen while moving
@@ -37,12 +63,31 @@ class MachineLaser:
         del self._laser
 
     def reinstate(self, x, y):
+        """
+            Reuses the existing sprite to fire and spawn the players laser on the screen.
+
+            :param x: The x-coordinate of the laser
+            :type x: float
+
+            :param y: The y-coordinate of the laser
+            :type y: float
+
+            :return: None
+        """
+
         self._laser.shape(machine_mode_setup.laser_texture)
         self._laser.goto(x, y)
 
     @property
     def laser(self):
+        """Machine Player Laser Getter"""
         return self._laser
 
     def remove(self):
+        """
+            Removes the players laser from the screen.
+
+            :return: None
+        """
+
         self.laser.hideturtle()
